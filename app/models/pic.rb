ec8f1b5a860747d73cb5db9dfcef9ec8e517352a
yashpriyam <yashpriyam74@gmail.com>
2019-06-22 18:17:08 +0530

    added image finally showing on the home page

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,6 +1,6 @@
 class Pic < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.jpg"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.jpg", validate_media_type: false
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
